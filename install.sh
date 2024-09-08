5a35e5288b282342ec0571ebb4b6d40c99d637b4
Mike Harrison <mike@nextwebtoday.com>
2024-09-08 19:54:26 -0400

    Exit on fail in install.sh and install dotenv with apt

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-sudo apt install python3-pip python3-rpi.gpio python3-w1thermsensor
+set -euo pipefail
 
-pip install python-dotenv
+sudo apt install python3-pip python3-rpi.gpio python3-w1thermsensor python3-dotenv
 
 python setup.py
 python main.py
