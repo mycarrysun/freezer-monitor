e825772511fe295460d7e2e3392e7429dae90c57
Mike Harrison <mike@nextwebtoday.com>
2024-09-08 20:01:30 -0400

    Use venv and create requirements.txt

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-sudo apt install python3-pip python3-rpi.gpio python3-w1thermsensor python3-dotenv
+sudo apt install python3-pip
+
+python -m venv venv
+source ./venv/bin/activate
+
+pip3 install -r requirements.txt
 
 python setup.py
 python main.py
