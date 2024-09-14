de3b603d04c2e6943be84e2f21ebb8c45dde10d8
Mike Harrison <mike@nextwebtoday.com>
2024-09-14 01:35:38 -0400

    Change to aws url

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ API_TOKEN = 'API_TOKEN'
 
 def make_api_request(path, method='POST', json={}):
     load_dotenv(override=True)
-    endpoint = f"{os.getenv('FREEZERBOT_API_HOST', 'https://freezerbot.askmeificare.duckdns.org')}/api/{path}"
+    endpoint = f"{os.getenv('FREEZERBOT_API_HOST', 'https://freezerbot.nextwebtoday.com')}/api/{path}"
     headers = {
         'Authorization': f"Bearer {os.getenv(API_TOKEN)}",
         'Accept': 'application/json',
